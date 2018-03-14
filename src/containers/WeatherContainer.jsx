6d47088cf8ac74ad1a63b5c9a3392a7f3adeaf9c
Vladimir Gornyy <vladimir.gornyy@flatstack.com>
2018-03-14 18:48:07 +0300

    Initial commit

---
+++ src/containers/WeatherContainer.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { fetchWeather } from "../services/api"
+
+class WeatherContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {city: "Kazan"};
+  }
+
+  componentDidMount() {
+    this.getWeather();
+  }
+
+  getWeather(searchedCity = this.state.city) {
+    fetchWeather(searchedCity)
+      .then((response) => {
+          var weather = response.list.map((dayWeather) => {
+            return {
+              dayWeather,
+              country: response.city.country,
+              city: response.city.name
+            }
+          });
+
+          this.setState({
+            weekWeather: weather,
+            city: this.state.searchedCity
+          });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+      {this.state.weekWeather && 
+        <ul>
+          {this.state.weekWeather.map((item,key) => (
+            <li key={key}>{`${new Date(item.dayWeather.dt*1000).toString().split(' ')[0]} ${item.dayWeather.temp.day}`}</li>
+          ))}
+        </ul>
+      }
+      </div>
+    );
+  }
+}
+
+export default WeatherContainer;
