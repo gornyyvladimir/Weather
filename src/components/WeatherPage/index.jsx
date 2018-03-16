4ec61664bba1a05b27531a12de0239eb9f8dfe60
Vladimir Gornyy <vladimir.gornyy@flatstack.com>
2018-03-16 19:38:32 +0300

    Add progressive image loading

--- src/components/WeatherPage/index.jsx
+++ src/components/WeatherPage/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProgressiveImage from 'react-progressive-image';
 import WheatherWeek from './WheatherWeek';
 
 const WeatherPage = ({weekWeather, city, hasError, onChange, country, image}) => {
@@ -11,7 +12,15 @@ const WeatherPage = ({weekWeather, city, hasError, onChange, country, image}) =>
       return <p>Loading ...</p>
     }
     else {
-      return <img src={ image.urls.regular } alt="snow"/>;
+      return (
+        <ProgressiveImage src={image.urls.regular} placeholder={image.urls.thumb}>
+          {(src, loading) => (
+            <div style={{overflow: `hidden`, width: `800px`, height: `600px`}}> 
+              <div style={{ filter: `blur(${loading ? 5 : 0}px)`, transform: `scale(1.1)`, background: `url(${src})`, backgroundSize: 'cover', width: `800px`, height: `600px`}}/>
+            </div>
+          )}
+        </ProgressiveImage>
+      );
     }
   };
 
