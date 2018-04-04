e76df1e565fd9ed739df2fde743cd2e336310d1a
Gornyy <vladimir.gornyy@flatstack.com>
2018-04-04 18:24:11 +0300

    Add proptypes to WheatherPage

--- src/components/WeatherPage/index.jsx
+++ src/components/WeatherPage/index.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import WheatherWeek from './WheatherWeek';
 import WheatherCover from './WheatherCover';
 import WheatherCard from './WheatherCard';
@@ -10,5 +11,19 @@ const WeatherPage = props => (
         {props.itemId != null && <WheatherCard {...props}/>}
       </Fragment>
 );
+
+WeatherPage.propTypes = {
+  inputValue: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  hasError: PropTypes.bool.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  itemId: PropTypes.number
+};
+
+WeatherPage.defaultProps = {
+  itemId: null
+}
 
 export default WeatherPage;
