07b197f58b0be3da222e031d60cc544794497456
Gornyy <vladimir.gornyy@flatstack.com>
2018-04-11 19:34:04 +0300

    Add transparent to background

--- src/components/ProgressiveBackground.jsx
+++ src/components/ProgressiveBackground.jsx
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 import breakpoint from 'styled-components-breakpoint';
 import ProgressiveImage from 'react-progressive-image';
 
+// transparent image for fade effect
+const transparent = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iOTAwIiBoZWlnaHQ9IjYwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48L3N2Zz4=';
+
 const Background = styled.div`
   ${breakpoint('tablet')`
     position: absolute;
@@ -11,7 +14,7 @@ const Background = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    background-image: ${props => (props.url ? `url(${props.url})` : 'none')};
+    background-image: ${props => (props.url ? `url(${props.url})` : `url(${transparent})`)};
     background-position: center center;
     background-size: cover;
     filter: blur(40px);
