abfabdaf322023df4c3141513103fd3a3478771c
= <=>
2018-10-11 11:08:44 +0300

    check if geocode working as expected

--- src/adapters/geocodeAdapter.js
+++ src/adapters/geocodeAdapter.js
@@ -1,12 +1,12 @@
 import geocodeApi from '../services/geocodeApi';
 
 export default async (lat, lng) => {
-  const geocodeObject = await geocodeApi(lat, lng);
-
-  const addressComponents = geocodeObject[0].address_components;
-  const city = addressComponents.filter(component => component.types.includes('locality', 'political'))[0].long_name;
-  const country = addressComponents.filter(component => component.types.includes('country', 'political'))[0].long_name;
-
+  const geocodeArray = await geocodeApi(lat, lng);
+  console.log(geocodeArray);
+  const geocodeObject = geocodeArray.reverse().find(component => component.types.includes('locality', 'political'));
+  console.log(geocodeObject);
+  const city = geocodeObject.address_components.filter(component => component.types.includes('locality', 'political'))[0].long_name;
+  const country = geocodeObject.address_components.filter(component => component.types.includes('country', 'political'))[0].long_name;
   return {
     city,
     country,
